fe5bb99c524c5398bf4799fc6c051c378586520b
DESKTOP-K1COMOS\esteb <services@yourvrexperience.com>
2023-04-02 18:18:04 +0200

    Missing interface.

---
+++ Utils/INetworkPickable.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+namespace yourvrexperience.Networking
+{
+	public interface INetworkPickable
+	{
+		bool ToggleControl();
+	}
+}
