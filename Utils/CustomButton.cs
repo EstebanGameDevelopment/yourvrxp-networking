bfbe2a185324707c013c549eeb6b303cfa095b4d
DESKTOP-K1COMOS\esteb <services@yourvrexperience.com>
2023-04-09 18:28:24 +0200

    Extended functionality.

--- Utils/CustomButton.cs
+++ Utils/CustomButton.cs
@@ -10,10 +10,30 @@ namespace yourvrexperience.Utils
     public class CustomButton : Button
     {
 		public Action<CustomButton> ClickedButton;
+		public Action<CustomButton> PointerDownButton;
+		public Action<CustomButton> PointerUpButton;
+		public Action<CustomButton> PointerEnterButton;
+		public Action<CustomButton> PointerExitButton;
 
 		public override void OnPointerClick(PointerEventData eventData)
 		{
 			ClickedButton?.Invoke(this);
+		}
+		public override void OnPointerDown(PointerEventData eventData)
+		{
+			PointerDownButton?.Invoke(this);
+		}
+        public override void OnPointerEnter(PointerEventData eventData)
+		{
+			PointerEnterButton?.Invoke(this);
+		}
+        public override void OnPointerExit(PointerEventData eventData)
+		{
+			PointerExitButton?.Invoke(this);
+		}
+        public override void OnPointerUp(PointerEventData eventData)
+		{
+			PointerUpButton?.Invoke(this);
 		}
     }
 }
